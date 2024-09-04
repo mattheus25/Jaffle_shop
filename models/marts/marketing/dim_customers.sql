4d2122fc821a0ed9900fe88e4c1a537222725e4b
mattheus  nascimento <mmsnascimento23@gmail.com>
2024-09-04 14:39:10 +0000

    reorganized folders and changed the some materalization from jinja for yml file

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,12 +1,9 @@
-{{
-   config (
-        materialized = 'table'
-          )
-}}
+
 
 with customers as (
 
     select * from {{ref('stg_jaffle_shop__customers')}}
+
 ),
 
 orders as (
@@ -19,11 +16,9 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
 
     group by 1
@@ -33,6 +28,7 @@ customer_orders as (
 final as (
 
     select
+
         customers.customer_id,
         customers.first_name,
         customers.last_name,
